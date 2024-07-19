c33afd0444dc71eb437ea8186a559d63532bbfd8
ezrahall1 <ezrahall87@gmail.com>
2024-07-19 16:20:29 +0100

    Created Azure function API

---
+++ backend/api/GetResumeCounter.cs
@@ -0,0 +1,31 @@
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Company.Function
+{
+    public static class GetResumeCounter
+    {
+        [FunctionName("GetResumeCounter")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "AzureResume",
+                collectionName: "Counter",
+                ConnectionStringSetting = "AzureResumeConnectionString",
+                Id = "1",
+                PartitionKey = "1")] Counter counter,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            counter.Count += 1;
+
+            return new OkObjectResult(counter);
+        }
+    }
+}
